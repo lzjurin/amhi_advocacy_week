c67f37638c460081ac5db628baee8253ceb5c437
Larry Zhang <larryzhang101@gmail.com>
2016-11-27 19:43:24 -0500

    Reorganizing (again) and setting up uploads

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+UPLOAD_FOLDER = '/uploads'
+FILTER = '/filters/filter_3.png'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No uploaded file')
+            return redirect(request.url)
+        f = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if f.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for(app.config['UPLOAD_FOLDER'],
+                                    filename=filename))
+    return render_template('home.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
