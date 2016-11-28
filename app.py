13a81ee9b2acb847ea00528c72bf28f394ed0afd
Larry Zhang <larryzhang101@gmail.com>
2016-11-27 20:42:27 -0500

    Finished uploading and filtering

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for
+import os, subprocess
+from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-UPLOAD_FOLDER = '/uploads'
-FILTER = '/filters/filter_3.png'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.debug=True
 
+# Configurations
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['OUT_FOLDER'] = 'out'
+app.config['FILTER'] = 'filters/filter_3.png'
+app.config['SCRIPT'] = 'scripts/overlay'
+app.secret_key ='asdlkjfho2837hlfbvhzv8o7n3af'
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -17,19 +22,21 @@ def home():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No uploaded file')
-            return redirect(request.url)
+            return render_template('failure.html')
         f = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
-        if f.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if f and allowed_file(f.filename):
-            filename = secure_filename(f.filename)
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for(app.config['UPLOAD_FOLDER'],
-                                    filename=filename))
+        if f.filename == '' or not allowed_file(f.filename):
+            return render_template('failure.html')
+
+        secure = secure_filename(f.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], secure)
+        f.save(path)
+        result = subprocess.call([app.config['SCRIPT'], path, app.config['FILTER'], os.path.join(app.config['OUT_FOLDER'], secure)])
+        if result:
+            flash("Invalid image :(", "Error")
+            return render_template('home.html')
+        return render_template(send_from_directory(directory=app.config['OUT_FOLDER'], filename=secure)
     return render_template('home.html')
 
 if __name__ == "__main__":
